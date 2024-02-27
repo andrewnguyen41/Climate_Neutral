4dedca857cedba4eea581c4044c18d39c4c3365a
Haari Sushil Raj <hsushilr2000@gmail.com>
2024-02-27 01:25:47 -0500

    Calculations And Data Flow

--- results.js
+++ results.js
@@ -1,50 +1,92 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var canvas1 = document.getElementById("Total Emissions By Vehicle");
-    var ctx1 = canvas1.getContext("2d");
-
-    var canvas2 = document.getElementById("Emissions Intensity By Vehicle");
-    var ctx2 = canvas2.getContext("2d");
-
-    var data1 = [
-        { label: "Vehicle 1", value: 150 },
-        { label: "Vehicle 2", value: 120 },
-        { label: "Vehicle 3", value: 100 },
-        { label: "Vehicle 4", value: 90 }
-    ];
-
-    var data2 = [
-        { label: "Vehicle 1", value: 100 },
-        { label: "Vehicle 2", value: 80 },
-        { label: "Vehicle 3", value: 150 },
-        { label: "Vehicle 4", value: 120 }
-    ];
-
-    var barWidth = 30;
-    var startX1 = 90;
-    var startY1 = 30;
-    var startX2 = 90;
-    var startY2 = 30;
-
-    data1.forEach(function(item) {
-        ctx1.fillStyle = "#0c1c81"; 
-        ctx1.fillRect(startX1, startY1, item.value, barWidth);
-
-        ctx1.fillStyle = "#007FFF"; 
+class VehicleCalculator {
+    constructor(annualFuelConsumption, annualVehicleKilometersTraveled, fuelEmissionsCoefficient) {
+        this.annualFuelConsumption = annualFuelConsumption;
+        this.annualVehicleKilometersTraveled = annualVehicleKilometersTraveled;
+        this.fuelEmissionsCoefficient = fuelEmissionsCoefficient;
+    }
+  
+    calculateFuelEfficiency() {
+        if (this.annualVehicleKilometersTraveled === 0) return Infinity;
+        return this.annualFuelConsumption / this.annualVehicleKilometersTraveled;
+    }
+  
+    calculateAnnualEmissions() {
+        return this.annualFuelConsumption * this.fuelEmissionsCoefficient;
+    }
+  
+    calculateEmissionsIntensity() {
+        if (this.annualVehicleKilometersTraveled === 0) return Infinity;
+        return this.calculateAnnualEmissions() / this.annualVehicleKilometersTraveled;
+    }
+  }
+  
+  function initPage() {
+    // Retrieve the vehicle data from localStorage
+    var vehicleData = JSON.parse(localStorage.getItem('vehicleData'));
+  
+    if (!vehicleData) {
+      console.log('No vehicle data found in localStorage.');
+      return; 
+    }
+  
+    const canvas1 = document.getElementById("TotalEmissionsByVehicle");
+    const ctx1 = canvas1.getContext("2d");
+  
+    const canvas2 = document.getElementById("EmissionsIntensityByVehicle");
+    const ctx2 = canvas2.getContext("2d");
+  
+    const barWidth = 30;
+    let startX1 = 90;
+    let startY1 = 30;
+    let startX2 = 90;
+    let startY2 = 30;
+  
+    vehicleData.forEach(function(vehicle, i) {
+        let fuelEmissionsCoefficient;
+  
+        if (vehicle.fuelType === "Gasoline") {
+          fuelEmissionsCoefficient = 2299;
+        } else if (vehicle.fuelType === "E10 Gasoline") {
+          fuelEmissionsCoefficient = 2071;
+        } else if (vehicle.fuelType === "Diesel") {
+          fuelEmissionsCoefficient = 2730;
+        } else {
+          fuelEmissionsCoefficient = 0;
+        }
+  
+        const vehicleCalculator = new VehicleCalculator(vehicle.annualFuelConsumption, vehicle.annualVehicleKilometersTraveled, fuelEmissionsCoefficient);
+  
+        const annualEmissionsValue = vehicleCalculator.calculateAnnualEmissions();
+        const emissionsIntensityValue = vehicleCalculator.calculateEmissionsIntensity();
+  
+        ctx1.fillStyle = "#0c1c81";
+        ctx1.fillRect(startX1, startY1, annualEmissionsValue, barWidth);
+  
+        ctx1.fillStyle = "#007FFF";
         ctx1.font = "14px Arial";
-        ctx1.fillText(item.label, 10, startY1 + barWidth / 2);
-
-
-        startY1 += 50; 
-    });
-
-    data2.forEach(function(item) {
-        ctx2.fillStyle = "#26B170"; 
-        ctx2.fillRect(startX2, startY2, item.value, barWidth);
-
-        ctx2.fillStyle = "#007FFF"; 
+        ctx1.fillText("Vehicle " + (i + 1), 10, startY1 + barWidth / 2);
+  
+        startY1 += 45;
+  
+        ctx2.fillStyle = "#26B170";
+        ctx2.fillRect(startX2, startY2, emissionsIntensityValue, barWidth);
+  
+        ctx2.fillStyle = "#007FFF";
         ctx2.font = "14px Arial";
-        ctx2.fillText(item.label, 10, startY2 + barWidth / 2);
-
-        startY2 += 50; 
+        ctx2.fillText("Vehicle " + (i + 1), 10, startY2 + barWidth / 2);
+  
+        startY2 += 45;
     });
-});
+  }
+  
+  initPage();
+  
+  function goNextStep() {
+    const data = {
+        AnnualEmissions: annualEmissionsValue,
+        EmissionsIntensity: emissionsIntensityValue
+    };
+    localStorage.setItem('step3', JSON.stringify(data));
+    goNext(3);
+  }
+  
