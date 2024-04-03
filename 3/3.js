4188f62db0d32a17c85f90b60839970116b350b2
Haari Sushil Raj <hsushilr2000@gmail.com>
2024-04-03 16:54:34 -0400

    Component 3 graph set for handling small and large values

--- 3/3.js
+++ 3/3.js
@@ -85,11 +85,39 @@ function initPage() {
       vehicleCalculator.calculateEmissionsIntensity() * quantity;
     console.log('EI' + emissionsIntensityValue);
 
+
     let annualEmissionsValueg;
-    if (vehicle.annualVKT <= 9999) {
+    if (annualEmissionsValue <= 999999) {
+      annualEmissionsValueg = (annualEmissionsValue / 10000) * 0.23;
+    } else if (annualEmissionsValue > 999999 && annualEmissionsValue <= 9999999) {
+      annualEmissionsValueg = (annualEmissionsValue / 10000) * 0.32;
+
+    }else if (annualEmissionsValue > 9999999) {
+      annualEmissionsValueg = (annualEmissionsValue / 100000) * 0.2;
+    }
+
+/*
+    let annualEmissionsValueg;
+    if (annualEmissionsValue <= 99999) {
       annualEmissionsValueg = (annualEmissionsValue / 10000) * 0.3;
+    } else if (annualEmissionsValue > 99999 && annualEmissionsValue <= 999999) {
+      annualEmissionsValueg = (annualEmissionsValue / 10000) * 0.3;
+    } else if (annualEmissionsValue > 999999) {
+      annualEmissionsValueg = (annualEmissionsValue / 100000) * 0.2;
+    }
+    */
+
+
+
+
+
+
+
+    let emissionsIntensityValueg;
+    if (emissionsIntensityValue <= 999) {
+      emissionsIntensityValueg = (emissionsIntensityValue / 10) * 0.4;
     } else {
-      annualEmissionsValueg = (annualEmissionsValue / 100000) * 0.7;
+      emissionsIntensityValueg = (emissionsIntensityValue / 10) * 0.2;
     }
 
     arr.push({
@@ -120,7 +148,7 @@ function initPage() {
     ctx2.fillRect(
       startX2,
       startY2,
-      (emissionsIntensityValue / 1000) * 60,
+      emissionsIntensityValueg,
       barWidth
     );
 
